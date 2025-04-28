c1071dd49939c1cc9ee7e680132c0538d9d0fa6a
Haku <hakudevtw@gmail.com>
2025-04-28 22:05:50 +0900

    feat: setup react-query

---
+++ src/components/providers.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default Providers;
