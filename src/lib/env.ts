f6f07e804422ef1b551f3bbdc086f569f0c9aed4
Haku <hakudevtw@gmail.com>
2025-05-03 14:41:18 +0900

    feat: add VITE_OMDB_API_KEY to environment configuration

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,8 +3,7 @@ import { z } from 'zod';
 const envSchema = z.object({
   MODE: z.union([z.literal('development'), z.literal('production')]).default('development'),
 
-  VITE_TMDB_API_KEY: z.string(),
-  VITE_TMDB_API_ACCESS_TOKEN: z.string(),
+  VITE_OMDB_API_KEY: z.string(),
 });
 
 const env = envSchema.parse(import.meta.env);
